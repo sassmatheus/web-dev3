115832355a2c751f6626ad8a1baec58ad33e8f99
Matheus Sass <matheus.sass@aluno.ifsp.edu.br>
2023-08-09 10:54:05 -0300

    segundo

---
+++ 2_segundo/controller/ctlHello.js
@@ -0,0 +1,14 @@
+
+const hello = (req, res) => (async () => {
+    res.json({ status: "ok", "mensagem": "Olá segundo!" });
+})();
+
+const helloUser = (request, res) => (async () => {
+    const { username } = request.body
+    res.json({ status: "ok", nomeusuario: username });
+})();
+
+module.exports = {
+    hello,
+    helloUser,
+}
